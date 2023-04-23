4da125463c9c56fecb04b9ed82f990d266f8ba9f
MadMoneyLab <119470128+MadMoneyLab@users.noreply.github.com>
2023-04-23 03:53:12 +0000

    config faucet contract import to faucet.js and app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { ethers } from "ethers";
+import faucetContract from "./ethereum/facuet";
 
 function App() {
   const [walletAddress, setWalletAddress] = useState("");
